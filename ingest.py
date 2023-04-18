39a8ae7e214edfdc77933851d08e9178a301dba7
石门 <xijieyin@gmail.com>
2023-04-18 21:07:46 +0800

    fix: use local file loader

--- ingest.py
+++ ingest.py
@@ -1,6 +1,6 @@
 """This is the logic for ingesting Notion data into LangChain."""
+from pathlib import Path
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import GitLoader
 import faiss
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
@@ -9,30 +9,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-loader = GitLoader(
-    clone_url="https://github.com/stonega/lushu-book",
-    repo_path="./lushu-book/",
-    branch="main",
-    file_filter=lambda file_path: file_path.endswith(".md")
-)
+# Here we load in the data in the format that Notion exports it in.
+ps = list(Path("lushu-book/").glob("**/*.md"))
 
-documents = loader.load()
+data = []
+sources = []
+for p in ps:
+    with open(p) as f:
+        data.append(f.read())
+    sources.append(p)
 
 # Here we split the documents, as needed, into smaller chunks.
 # We do this due to the context limits of the LLMs.
 text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
-docs = text_splitter.split_documents(documents)
 
-# docs = []
-# metadatas = []
-# for i, d in enumerate(data):
-#     splits = text_splitter.split_text(d)
-#     docs.extend(splits)
-#     metadatas.extend([{"source": sources[i]}] * len(splits))
+docs = []
+metadatas = []
+for i, d in enumerate(data):
+    splits = text_splitter.split_text(d)
+    docs.extend(splits)
+    metadatas.extend([{"source": sources[i]}] * len(splits))
 
 
 # Here we create a vector store from the documents and save it to disk.
-store = FAISS.from_documents(docs, OpenAIEmbeddings(), metadatas=metadatas)
+store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
 faiss.write_index(store.index, "docs.index")
 store.index = None
 with open("faiss_store.pkl", "wb") as f:
--- qa.py
+++ qa.py
@@ -4,6 +4,9 @@ from langchain import OpenAI
 from langchain.chains import VectorDBQAWithSourcesChain
 import pickle
 import argparse
+from dotenv import load_dotenv
+
+load_dotenv()
 
 parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
 parser.add_argument('question', type=str, help='The question to ask the notion DB')
@@ -16,7 +19,7 @@ with open("faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
 
 store.index = index
-chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0.5), vectorstore=store)
 result = chain({"question": args.question})
 print(f"Answer: {result['answer']}")
 print(f"Sources: {result['sources']}")

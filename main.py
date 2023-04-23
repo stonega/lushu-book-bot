da50069b1ba7413c0dd0286c312a701675ee8a28
stonega <xijieyin@gmail.com>
2023-04-23 21:28:33 +0800

    feat: add api

--- main.py
+++ main.py
@@ -1,48 +1,21 @@
-"""Python file to serve as the frontend"""
-import streamlit as st
-from streamlit_chat import message
-import faiss
-from langchain import OpenAI
-from langchain.chains import VectorDBQAWithSourcesChain
-import pickle
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from qa import ask_question
 
-# Load the LangChain.
-index = faiss.read_index("docs.index")
+load_dotenv()
 
-with open("faiss_store.pkl", "rb") as f:
-    store = pickle.load(f)
+app = FastAPI()
 
-store.index = index
-chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+class Item(BaseModel):
+    question: str
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-# From here down is all the StreamLit UI.
-st.set_page_config(page_title="Blendle Notion QA Bot", page_icon=":robot:")
-st.header("Blendle Notion QA Bot")
-
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-
-if "past" not in st.session_state:
-    st.session_state["past"] = []
-
-
-def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
-    return input_text
-
-
-user_input = get_text()
-
-if user_input:
-    result = chain({"question": user_input})
-    output = f"Answer: {result['answer']}\nSources: {result['sources']}"
-
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state["generated"]:
-
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+@app.post("/ask")
+def root(ask: Item):
+    question = ask.question
+    result = ask_question(question)
+    return result
--- qa.py
+++ qa.py
@@ -1,25 +1,42 @@
-"""Ask a question to the notion database."""
-import faiss
-from langchain import OpenAI
-from langchain.chains import VectorDBQAWithSourcesChain
-import pickle
+from langchain import FAISS, OpenAI, PromptTemplate
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 import argparse
 from dotenv import load_dotenv
 
-load_dotenv()
+def ask_question(question):
 
-parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
-parser.add_argument('question', type=str, help='The question to ask the notion DB')
+    load_dotenv()
+
+    embeddings = OpenAIEmbeddings()
+    db = FAISS.load_local('faiss_index', embeddings)
+    
+    # question_prompt_template = """Use the following pieces of context to answer the question at the end, 请必须使用中文回答。
+    # {context}
+    # Question: {question}
+    # :"""
+    # QUESTION_PROMPT = PromptTemplate(
+    #     template=question_prompt_template, input_variables=["context", "question"]
+    # )
+
+    qa_chain = load_qa_with_sources_chain(OpenAI(
+        temperature=0.2), chain_type="map_reduce")
+
+    chain = RetrievalQAWithSourcesChain(
+        combine_documents_chain=qa_chain, retriever=db.as_retriever())
+
+    result = chain({"question": question}, return_only_outputs=True)
+    return result
+
+
+parser = argparse.ArgumentParser(
+    description='Ask a question to the Lushu Book.')
+parser.add_argument('question', type=str,
+                    help='The question to ask the Lushu Book')
 args = parser.parse_args()
 
-# Load the LangChain.
-index = faiss.read_index("docs.index")
+result = ask_question(args.question)
 
-with open("faiss_store.pkl", "rb") as f:
-    store = pickle.load(f)
-
-store.index = index
-chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0.5), vectorstore=store)
-result = chain({"question": args.question})
 print(f"Answer: {result['answer']}")
 print(f"Sources: {result['sources']}")

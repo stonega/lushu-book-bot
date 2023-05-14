f64ad6b73bb16c657cb58c5a672cf5a98cb69795
stonega <xijieyin@gmail.com>
2023-05-14 22:32:32 +0800

    fix: use gpt-3.5-turbo

--- ask.py
+++ ask.py
@@ -1,5 +1,6 @@
 from langchain import FAISS, OpenAI, PromptTemplate
 from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from dotenv import load_dotenv
@@ -9,8 +10,8 @@ load_dotenv()
 embeddings = OpenAIEmbeddings()
 db = FAISS.load_local('faiss_index', embeddings)
 
-qa_chain = load_qa_with_sources_chain(OpenAI(
-    temperature=0.2), chain_type="map_reduce")
+qa_chain = load_qa_with_sources_chain(ChatOpenAI(
+    temperature=0.2, model_name="gpt-3.5-turbo"), chain_type="map_reduce")
 
 chain = RetrievalQAWithSourcesChain(
     combine_documents_chain=qa_chain, retriever=db.as_retriever())
